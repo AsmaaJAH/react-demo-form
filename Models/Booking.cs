8dfb2210a430f8b063564b9f313dfd38f01bdde7
AsmaaJAH <asmaagamal.nagy@gmail.com>
2024-11-04 13:42:40 +0200

    react MVC final commit

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -3,8 +3,8 @@ namespace RoombBooking.Models
     public class Booking
     {
         public int Id { get; set; }
-        public string Phone { get; set; }
-        public string Room { get; set; }
+        public required string Phone { get; set; }
+        public required string Room { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
     }
